319ab39b169fa3794c0bd7ee644a8cb0363d72d8
Pavel Kasyanov <bmp1@yandex.ru>
2017-06-05 14:58:57 +0300

    remove South migrations, add Payment.user related_name attribute

--- yandex_money/models.py
+++ yandex_money/models.py
@@ -63,7 +63,8 @@ class Payment(models.Model):
 
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, blank=True, null=True,
-        verbose_name=u'Пользователь')
+        verbose_name=u'Пользователь',
+        related_name='payments')
     pub_date = models.DateTimeField(u'Время создания', auto_now_add=True)
 
     # Required request fields
